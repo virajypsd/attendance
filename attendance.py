b95373dd3f46db25e15b0d0bd6242181a58f754a
virajypsd <vrajyadav048@gmail.com>
2025-03-26 08:40:10 +0530

    Update attendance.py

--- attendance.py
+++ attendance.py
@@ -1,16 +1,4 @@
-import firebase_admin
-from firebase_admin import credentials, db
-import streamlit as st
-
-# Initialize Firebase
-cred = credentials.Certificate("serviceAccountKey.json")
-firebase_admin.initialize_app(cred, {'databaseURL': 'https://attendance-system-1f6c4-default-rtdb.firebaseio.com/'})
-
-# Fetch attendance data
-ref = db.reference("attendance")
-data = ref.get()
-
-# Display data in Streamlit Web App
-st.title("Real-time Attendance Data")
-st.write("Live data from Firebase:")
-st.write(data)
+import pandas as pd
+url = "http://192.168.0.168:5000/get_attendance"
+df = pd.read_csv(url)
+print(df)
